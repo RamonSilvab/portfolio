0fb8b04e3a0a5e9ae29c4c9d428a71f0b98817ea
Ramon Silva Barros <ramon@dcfdci.fdci.br>
2024-07-09 20:02:24 -0300

    feat: criando a estrutura da pagina

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/ramonsilvab.png" />
+    <Titulo>Ramon Silva</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,14 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre</Titulo>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere iure
+      quidem impedit iste dolore delectus odio excepturi ex quae! Vitae aliquid
+      quis tempore a, fuga temporibus nam nostrum itaque laudantium!
+    </p>
+  </section>
+)
+
+export default Sobre
