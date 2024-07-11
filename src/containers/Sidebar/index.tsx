ba658702159352276d3c315ceaa9cb068cda1753
Ramon Silva Barros <ramon@dcfdci.fdci.br>
2024-07-10 21:12:29 -0300

    feat: estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import Avatar from '../../components/avatar'
+import { BotaoTema, Descricao, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/ramonsilvab.png" />
-    <Titulo fontSize={20}>Ramon Silva</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Ramon Silva</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        RamonSilvaB
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Dev FullStack Java
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
